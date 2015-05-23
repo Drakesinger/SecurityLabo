ece1bf6aee9f02af9513272957a920c2f793605c
fa18swiss <fa18swiss@users.noreply.github.com>
2015-05-23 10:14:41 +0200

    add lock

--- 02-challange-response/IPFailCounter.py
+++ 02-challange-response/IPFailCounter.py
@@ -1,10 +1,16 @@
+import threading
+
 class IPFailCounter:
     def __init__(self, max):
         self.__dic = {}
         self.__max = max
+        self.lock = threading.RLock()
     def isBlocked(self, ip):
-        return self.__dic.get(ip, 0) >= self.__max
+        with self.lock:
+            return self.__dic.get(ip, 0) >= self.__max
     def success(self, ip):
-        self.__dic[ip] = 0
+        with self.lock:
+            self.__dic[ip] = 0
     def fail(self, ip):
-        self.__dic[ip] = self.__dic.get(ip, 0) + 1
+        with self.lock:
+            self.__dic[ip] = self.__dic.get(ip, 0) + 1
